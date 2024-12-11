19a991df59fe745e4cad4b694ed7ca1313ac4eb2
Sayyed Uzer <uzairsayyad85@gmail.com>
2024-12-11 14:26:31 +0530

    fixed bug

--- src/App-v1.jsx
+++ src/App-v1.jsx
@@ -25,7 +25,7 @@ function Intro() {
       <p>
         Full-stack web developer and teacher at Udemy. When not coding or
         preparing a course, I like to play board games, to cook (and eat), or to
-        just enjoy the Portuguese sun at the beach.
+        just enjoy the Portuguese sun at the beach..
       </p>
     </div>
   );
